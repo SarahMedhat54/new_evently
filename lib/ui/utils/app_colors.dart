02fd219c886a83fdd5f3b977da052afd783ea70d
Ahmed Nabil <ahmed.nabil@kazyon.com>
2026-01-11 20:07:22 +0200

    Create core files - assets - styles - colors.

---
+++ lib/ui/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors{
+  static final Color blue = Color(0xff0E3A99);
+  static final Color white = Color(0xffFFFFFF);
+  static final Color offWhite = Color(0xffF4F7FF);
+  static final Color red = Color(0xffFF3232);
+  static final Color grey = Color(0xff686868);
+  static final Color black = Color(0xff1C1C1C);
+}
